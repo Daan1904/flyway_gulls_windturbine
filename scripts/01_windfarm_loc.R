bc2cb5780aca337ceb15f08fc73aa1ba66d0a2e9
Daan <d.kroeze.1@student.rug.nl>
2024-12-16 10:02:19 +0100

    Selected and filtered the input data

--- scripts/01_windfarm_loc.R
+++ scripts/01_windfarm_loc.R
@@ -10,9 +10,15 @@ library(lubridate)
 options(dplyr.width = Inf)
 
 #######################################################
-### Read file 1
-wind <- read.csv("C:/Users/daank/OneDrive - University of Twente/Documents/Rijksuniversiteit Groningen/Year 1 - 24-25/Flyway Ecology/Mini project - Gull migration wind farms/Global-Wind-Power-Tracker-June-2024.csv")
+### Read file
+wind <- read.csv("C:/Users/daank/OneDrive - University of Twente/Documents/Rijksuniversiteit Groningen/Year 1 - 24-25/Flyway Ecology/Mini project - Gull migration wind farms/Global-Wind-Power-Tracker-June-2024.csv") |>
+  dplyr::select(Country.Area, Project.Name, Capacity..MW., Installation.Type, Status, Start.year,
+                Latitude, Longitude, Location.accuracy, Wiki.URL) |>
+  dplyr::filter(Start.year <= 2024, Status == "operating") |>
+  as_tibble()
 wind
 
+### Save file
+write.csv(wind, "C:/Users/daank/OneDrive - University of Twente/Documents/Rijksuniversiteit Groningen/Year 1 - 24-25/Flyway Ecology/Mini project - Gull migration wind farms/windfarm_loc.csv", row.names = FALSE)
 
 
